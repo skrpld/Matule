d5a0e39ad7ec4fdd3f0c8ad2c9311ebb6060cc9f
skrpld <skrpld96@gmail.com>
2025-12-06 11:41:55 +0300

    Create base navigation
    make destinations, and base navigati0n

---
+++ app/src/main/java/com/skrpld/matule/ui/navigation/AppDestinations.kt
@@ -0,0 +1,21 @@
+package com.skrpld.matule.ui.navigation
+
+object AppDestinations {
+    // Auth flow
+    const val SPLASH_ROUTE = "splash_screen"
+    const val LOCK_ROUTE = "lock_screen"
+    const val LOGIN_ROUTE = "login_screen"
+    const val SIGNUP_PROFILE_ROUTE = "register_profile_screen"
+    const val SIGNUP_PASSWORD_ROUTE = "register_password_screen"
+    const val SIGNUP_LOCK_ROUTE = "register_lock_screen"
+
+    // Main flow
+    const val HOME_ROUTE = "home_screen"
+    const val CATALOG_ROUTE = "catalog_screen"
+    const val CART_ROUTE = "cart_screen"
+    const val PROFILE_ROUTE = "profile_screen"
+
+    // Projects flow
+    const val PROJECTS_ROUTE = "projects_screen"
+    const val CREATE_PROJECT_ROUTE = "create_screen"
+}
---
+++ app/src/main/java/com/skrpld/matule/ui/navigation/AppNavigation.kt
@@ -0,0 +1,75 @@
+package com.skrpld.matule.ui.navigation
+
+import androidx.navigation.NavHostController
+
+class AppNavigation(
+    private val navController: NavHostController
+) {
+    /**
+     * App start
+     */
+    fun navigateToSplash() {
+        navController.navigate(AppDestinations.SPLASH_ROUTE)
+    }
+
+    fun navigateToLock() {
+        navController.navigate(AppDestinations.LOCK_ROUTE)
+    }
+
+    /**
+     * Login / Signup
+     */
+    fun navigateToLogin() {
+        navController.navigate(AppDestinations.LOGIN_ROUTE) {
+            popUpTo(navController.graph.id) {
+                inclusive = true
+            }
+        }
+    }
+
+    fun navigateToSignupProfile() {
+        navController.navigate(AppDestinations.SIGNUP_PROFILE_ROUTE)
+    }
+
+    fun navigateToSignupPassword() {
+        navController.navigate(AppDestinations.SIGNUP_PASSWORD_ROUTE)
+    }
+
+    fun navigateToSignupLock() {
+        navController.navigate(AppDestinations.SIGNUP_LOCK_ROUTE)
+    }
+
+    /**
+     * Main
+     */
+    fun navigateToHome() {
+        navController.navigate(AppDestinations.HOME_ROUTE)
+    }
+
+    fun navigateToCatalog() {
+        navController.navigate(AppDestinations.CATALOG_ROUTE)
+    }
+
+    fun navigateToCart() {
+        navController.navigate(AppDestinations.CART_ROUTE)
+    }
+
+    fun navigateToProfile() {
+        navController.navigate(AppDestinations.PROFILE_ROUTE)
+    }
+
+    fun navigateToProjects() {
+        navController.navigate(AppDestinations.PROJECTS_ROUTE)
+    }
+
+    fun navigateToCreateProject() {
+        navController.navigate(AppDestinations.CREATE_PROJECT_ROUTE)
+    }
+
+    /**
+     * Utils
+     */
+    fun onBack() {
+        navController.popBackStack()
+    }
+}
