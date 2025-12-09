e1563d1325e8a469bea859218f94f9f10d7a54e4
skrpld <skrpld96@gmail.com>
2025-12-09 23:57:55 +0300

    Create independent components

---
+++ app/src/main/java/com/skrpld/matule/ui/components/auth/NextButton.kt
@@ -0,0 +1,37 @@
+package com.skrpld.matule.ui.components.auth
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun NextButton(
+    text: String = "Далее",
+    isFormValid: Boolean,
+    onClick: () -> Unit,
+) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp),
+        shape = RoundedCornerShape(12.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = if (isFormValid) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.secondary,
+            contentColor = if (isFormValid) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.onSecondary
+        )
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.titleSmall
+        )
+    }
+}
