03c7119f43c734947954629f6d996d888b885709
skrpld <skrpld96@gmail.com>
2025-12-23 11:40:46 +0300

    Create UIKit module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,4 +69,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation(project(":uikit"))
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "Matule"
 include(":app")
- 
+include(":UIKIT")
